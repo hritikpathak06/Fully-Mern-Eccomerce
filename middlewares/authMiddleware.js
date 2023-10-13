4b7cd0f82ee770f96f9335665bb2dfe5f905282f
Ritik_Kumar_Pathak <111448294+hritikpathak06@users.noreply.github.com>
2023-10-14 02:01:09 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ import userModel from "../models/userModel.js";
 // protected route token based
 export const requireSignIn = async(req,res,next) => {
     try {
-       const decode = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+       const decode = JWT.verify(req.headers.authorization, "ABCFRYAHKAJAKHJAKHAAKJAOOJUAIAOJUIAUOIAOI")
        req.user = decode;
        next();
     
